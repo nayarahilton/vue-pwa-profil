8741bda85d7ca5ed1e1ef01c3c7b73841ab6a3ae
lucasjs <lucas.tcjb@gmail.com>
2017-11-06 05:14:07 -0200

    Edit ESLint #5

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,44 @@
 // http://eslint.org/docs/user-guide/configuring
 
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
-  },
-  env: {
-    browser: true,
-  },
-  extends: 'airbnb-base',
-  // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
-  // check if imports actually resolve
-  'settings': {
-    'import/resolver': {
-      'webpack': {
-        'config': 'build/webpack.base.conf.js'
-      }
-    }
-  },
-  // add your custom rules here
-  'rules': {
-    // profil custom rules
-    "linebreak-style": 0,
-    "indent": [0, "tab"],
-    "no-tabs": 0,
-    "consistent-return": 0,
-    // don't require .vue extension when importing
-    'import/extensions': ['error', 'always', {
-      'js': 'never',
-      'vue': 'never'
-    }],
-    // allow optionalDependencies
-    'import/no-extraneous-dependencies': ['error', {
-      'optionalDependencies': ['test/unit/index.js']
-    }],
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
+	root: true,
+	parser: 'babel-eslint',
+	parserOptions: {
+		sourceType: 'module'
+	},
+	env: {
+		browser: true,
+	},
+	extends: 'airbnb-base',
+	// required to lint *.vue files
+	plugins: [
+		'html'
+	],
+	// check if imports actually resolve
+	'settings': {
+		'import/resolver': {
+			'webpack': {
+				'config': 'build/webpack.base.conf.js'
+			}
+		}
+	},
+	// add your custom rules here
+	'rules': {
+		// profil custom rules
+		"linebreak-style": 0,
+		"indent": [2, "tab"],
+		"no-tabs": 0,
+		"consistent-return": 0,
+		// don't require .vue extension when importing
+		'import/extensions': ['error', 'always', {
+			'js': 'never',
+			'vue': 'never'
+		}],
+		// allow optionalDependencies
+		'import/no-extraneous-dependencies': ['error', {
+			'optionalDependencies': ['test/unit/index.js']
+		}],
+		// allow debugger during development
+		'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+	}
 }
