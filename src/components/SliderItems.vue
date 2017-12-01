405f0205ec646eecd43e53b99699d70d8430aed3
Nayara Hilton <naylop@gmail.com>
2017-12-01 21:27:35 -0200

    slide test

--- src/components/SliderItems.vue
+++ src/components/SliderItems.vue
@@ -1,31 +1,51 @@
 <template>
-		<div class="slider">
-		<ul class="slider_list">
-			<li class="slider_list-item" v-for="item in items">
+	<div class="slider">
+		<swiper :options="swiperOption" >
+			<swiper-slide v-for="(item, index) in items">
 				<div class="slider_img-holder">
 					<img class="slider_img" src="http://nosrc.fbiz.com.br/640x480/ddd/777" />
 				</div>
-				<p class="slider_text">{{item.text}}</p>
-			</li>
-		</ul>
+				<p class="slider_text">{{item}}</p>
+			</swiper-slide>
+		</swiper>
 	</div>
 </template>
 
 <script>
+import Vue from 'vue';
+import VueAwesomeSwiper from 'vue-awesome-swiper';
+
+
+Vue.use(VueAwesomeSwiper, this.swiperOption);
+
+
 export default {
 	data() {
 		return {
 			items: [
-				{ text: '@nayarahilton' },
-				{ text: '@prisantos' },
-				{ text: '@lucasilva' },
-				{ text: '@lilisantos' },
-				{ text: '@vinicuisbueno' },
-				{ text: '@gabriellopes' },
-				{ text: '@rachelaquino' },
-				{ text: 'Outro' },
-				{ text: 'Outro' },
+				'@nayarahilton',
+				'@prisantos',
+				'@lucasilva',
+				'@lilisantos',
+				'@vinicuisbueno',
+				'@gabriellopes',
+				'@rachelaquino',
+				'@nayarahilton',
+				'@prisantos',
+				'@lucasilva',
+				'@nayarahilton',
+				'@prisantos',
+				'@lucasilva 2',
 			],
+			swiperOption: {
+				slidesPerView: 'auto',
+				spaceBetween: 0,
+				pagination: {
+					el: '.swiper-pagination',
+					clickable: true,
+				},
+			},
+
 		};
 	},
 };
@@ -34,21 +54,20 @@ export default {
 
 <style lang="stylus">
 	@import '../assets/styles/*'
+	@import '../../node_modules/swiper/dist/css/swiper.css';
+
+	.swiper-container
+		holder()
+		overflow initial
+
+	.swiper-slide
+		width 80px
+		padding 20px 10px
+		background #f4f4f4
+		text-align center
+		overflow hidden
 
 	.slider
-		padding 20px 0
-		box-sizing border-box
-		background #fff
-
-		&_list
-			nowrap-list()
-			holder()
-
-		&_list-item
-			nowrap-list-item()
-			min-width 60px
-			max-width 100px
-
 		&_text
 			margin-bottom 0
 			margin-top 0px
@@ -60,6 +79,7 @@ export default {
 			height 50px
 			border-radius 50%
 			background #fff
+			margin 0 auto
 
 		&_img
 			object-fit cover
